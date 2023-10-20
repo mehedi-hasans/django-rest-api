029d814db1d5eb1675dfd62f716edd0e05844ceb
Mehedi Hasan <shihab3676@gmail.com>
2023-10-20 16:52:18 +0600

    View Template Model Data

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index(request):
+    musicianList = Musician.objects.order_by('first_name')
+    diction = {'musician': musicianList}
+    return render(request, 'index.html', context= diction)
