c094d3df291a16a354ce083ec96d283c69c57c51
Mehedi Hasan <shihab3676@gmail.com>
2023-10-20 17:37:54 +0600

    Crating a Django Form P2 Label, value

--- api/forms.py
+++ api/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class djangoForms(forms.Form):
-    name= forms.CharField()
-    email= forms.EmailField()
+    name= forms.CharField(label='User Name')
+    email= forms.EmailField(label='User Email')
