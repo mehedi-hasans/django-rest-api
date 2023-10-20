05c47c740f1d25a5516c8863524aa782f08e1ff6
Mehedi Hasan <shihab3676@gmail.com>
2023-10-20 17:50:30 +0600

    Crating a Django Form P2 .as_p, widget, dob

--- api/forms.py
+++ api/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class djangoForms(forms.Form):
-    name= forms.CharField(label='User Name')
+    name= forms.CharField(label='User Name', widget=forms.TextInput(attrs={'placeholder': 'Enter your name', 'style': 'width: 320px'}))
+    dob = forms.DateField(label='Date of Birth:', widget=forms.TextInput(attrs={'type': 'date'}))
     email= forms.EmailField(label='User Email')
